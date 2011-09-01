023c92b45452386dd4c7e984542db3b69c23a29b
Sergej Felde <sfelde@gmail.com>
2011-09-01 16:56:08 +0200

    move function includeHtml to plugin.js

--- js/plugins.js
+++ js/plugins.js
@@ -9,3 +9,15 @@ window.log = function(){
   }
 };
 (function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
+
+
+function includeHtml(container,url,content, insert){
+	$(container).html('');
+	$(container).load(url, function(data) {
+		if (insert == 'after'){
+			$(content).after(data);
+		}else{
+			$(content).before(data);
+		}
+	});		
+}
--- js/script.js
+++ js/script.js
@@ -1,21 +1,9 @@
 jQuery(document).ready(function($){  
 	var _getContainer = $('#loadContent').hide();
 	var _content = $('#main');
-	function includeHtml(container,url,content, insert){
-		$(container).html('');
-		$(container).load(url, function(data) {
-			if (insert == 'after'){
-				$(content).after(data);
-			}else{
-				$(content).before(data);
-			}
-		});		
-	}
-	
-	
 	
 	includeHtml(_getContainer,'inc/header.html',_getContainer,'before');
-		includeHtml(_getContainer,'inc/main.html',_getContainer,'before');
+	includeHtml(_getContainer,'inc/main.html',_getContainer,'before');
 	includeHtml(_getContainer,'inc/footer.html',_getContainer,'after');
 	    
 	//console.log(_content);
